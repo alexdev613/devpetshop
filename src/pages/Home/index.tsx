c3cbac2dba7909a13c293a11310ab2b4409ce03a
Alex Nascimento <alexjose68@hotmail.com>
2024-06-14 16:53:08 -0300

    feat: passei o arquivo db.json para dentro do diretório raiz do projeto, instalei o axios para criar e configurar o arquivo do meu serviço de requisição http a partir da baseURL que é o que o json-server me passa quando roda o db.json, criei um useEffect com uma função async para obter a resposta da minha api toda vez que a página for montada. A partir de um useState chamado products eu peguei os dados da resposta da api para preencher o array desse useState também criando uma tipagem com os tipos de dados que recebo no json da api e passei o que peguei com o useState para renderizar na página Home através de um map usando como base o card que criei no commit anterior

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,59 @@
+import { useEffect, useState } from "react";
 import { BsCart3 } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
+import { api } from "../../services/api";
+
+interface ProductsProps {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
+
 export function Home() {
+  const [products, setProducts] = useState<ProductsProps[]>([]);
+  
+  useEffect(() => {
+    async function getProducts() {
+      const resposta = await api.get("/products");
+      setProducts(resposta.data)
+    }
+
+    getProducts();
+  }, []);
+
   return(
     <div>
-      <main className="w-full max-w-7xl px-4 mx-auto ">
+      <main className="w-full max-w-7xl px-4 mx-auto pb-8">
         <h1 className="text-2xl font-bold text-center mt-10 mb-7" >Seleção de produtos</h1>
 
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
           
-          <section className="w-full border-2 border-slate-500 rounded-lg px-1 py-4 bg-gray-100">
+          {products.map( (productItem) => (
+            <section key={productItem.id} className="w-full border-2 border-slate-500 rounded-lg px-1 py-4 bg-gray-100">
             <Link to="/">
               <img
                 className="w-full rounded-xl mb-2 border-2 border-gray-200/30"
-                src="https://sujeitoprogramador.com/wp-content/uploads/2023/06/racao4.png"
-                alt=""
+                src={productItem.cover}
+                alt={productItem.title}
               />
-            <p className="font-medium mt-1 mb-2 text-center">Ração Royal Canin 10kg</p>
+            <p className="font-medium mt-1 mb-2 text-center">{productItem.title}</p>
             </Link>
 
             <div className="flex gap-2 items-center justify-center">
-              <strong>R$ 21,90</strong>
+              <strong>
+                {productItem.price.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}
+              </strong>
+              
               <button className="bg-green-800 rounded-xl p-1.5 border-2 border-green-500">
                 <BsCart3 size={18} color="#FFF"/>
               </button>
             </div>
 
           </section>
+          ))}
           
 
         </div>
